bacd92175d90d7696a8fe17f7f0a3fa684093538
Jude G <judith.gammie@gmail.com>
2015-03-17 20:39:25 +1100

    changed test

--- spec/robocop_spec.rb
+++ spec/robocop_spec.rb
@@ -3,7 +3,7 @@ require 'robocop'
 RSpec.describe Robocop do
   it "should beep" do
     d = Robocop.new
-    expect(d.beep).to eq('beep boop')
+    expect(d.beep).to eq('beep beep boop')
     print "Hello World!"
   end
 end
