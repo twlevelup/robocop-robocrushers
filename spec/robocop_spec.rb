0b41f77f84c529ddcb4e4f5886bf9691d674cc34
mwx789 <myliving21@yahoo.co.id>
2015-03-30 23:25:21 +1100

    merge

--- spec/robocop_spec.rb
+++ spec/robocop_spec.rb
@@ -1,9 +1,9 @@
 require 'robocop'
 
 RSpec.describe Robocop do
-  it "should beep" do
+  it 'should beep' do
     d = Robocop.new
     expect(d.beep).to eq('beep boop')
-    print "Hello World!"
+    print 'Hello World!
   end
 end
