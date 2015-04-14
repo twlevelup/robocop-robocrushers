6ec1cd28a5767210a507a6ca10d1dfdbf9ba1b5e
arvinwiyono <arvinwiyono@gmail.com>
2015-04-14 19:52:58 +1000

    move_backward! out of bound printed error message

--- lib/robocop.rb
+++ lib/robocop.rb
@@ -59,18 +59,26 @@ class Robocop
         if @direction == :N
             if check_within_boundary?(@x, @y-1)
                 @y -= 1
+            else
+                print_outofbounds
             end
         elsif @direction == :E
             if check_within_boundary?(@x-1, @y)
                 @x -= 1
+            else
+                print_outofbounds
             end
         elsif @direction == :S
             if check_within_boundary?(@x, @y+1)
                 @y += 1
+            else
+                print_outofbounds
             end
         elsif @direction == :W
             if check_within_boundary?(@x+1, @y)
                 @x += 1
+            else
+                print_outofbounds
             end
         end
     end  
