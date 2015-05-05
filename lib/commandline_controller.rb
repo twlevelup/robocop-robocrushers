4db28707115ac2d08e836f8bfd861e10c235f80b
Jude G <judith.gammie@gmail.com>
2015-05-05 18:59:16 +1000

    integrated grid to controller, accept input of robot identifier and required command

--- lib/commandline_controller.rb
+++ lib/commandline_controller.rb
@@ -1,12 +1,17 @@
 require './robocop'
 require './command'
+require './grid'
 
 robot = Robocop.new(0, 0, :N)
+city_grid = Grid.new()
 
 puts "Welcome to Robocop-Robocrushers!"
 while true do 
-	print "Command: "
+    print "Command: "
 	cmd = gets.chomp.to_s
-	newCmd = Command.new(cmd)
-	robot.interpret(newCmd.get_representation)	
+	cmd = cmd.split(", ")
+    newCmd = Command.new(cmd[1])
+    current_robot = city_grid.get_robot(cmd[0])
+    current_robot.interpret(newCmd.get_representation)
+
 end
