fcb5203210b11e7962520a460b1d99b2e4a1ed61
Jude G <judith.gammie@gmail.com>
2015-05-14 18:56:41 +1000

    fixed the help menu to include print grid option

--- lib/command.rb
+++ lib/command.rb
@@ -25,6 +25,6 @@ class Command
     end
 
 	def show_help
-		"\n\n======================================================\nUsage:\n\tRobocop: (robocop identifier), (command)\n\tSystem: (command)\n\nRobocop Operations:\n\tl - Turn left\n\tr - Turn right\n\tf - Move forward\n\tb - Move backward\n\ta - Make arrest\n\nSystem Operations\n\tv - Print the location of all robocops\n\th - Show help screen\n\tq - Quit\n======================================================\n\n"
+		"\n\n======================================================\nUsage:\n\tRobocop: (robocop identifier), (command)\n\tSystem: (command)\n\nRobocop Operations:\n\tl - Turn left\n\tr - Turn right\n\tf - Move forward\n\tb - Move backward\n\ta - Make arrest\n\nSystem Operations\n\tv - List the location of all robocops\n\tp - Print the grid with robot locations \n\th - Show help screen\n\tq - Quit\n======================================================\n\n"
 	end
 end
